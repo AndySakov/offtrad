1297a6d2e50186e1315518099c2f451856029218
Obafemi Teminife <andysakov1958@gmail.com>
2021-03-12 16:16:20 +0100

    search bar implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { isMobile } from 'react-device-detect'
 
-import './App.scss'
+import './styles/styles.scss';
 import DesktopView from './views/DesktopView'
 import MobileView from './views/MobileView'
-// import ReactDOM from 'react-dom'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+
+const clearSearch = () => {
+  document.getElementsByClassName("search-bar")[0].value = ""
+}
 
 const SearchBar = () => {
   return (
-    <>
-      <input type="text" className="search-bar" placeholder="Search..."/>
-    </>
+    <div className='search-bar-wrapper'>
+      <input type="text" className="search-bar" placeholder='Search for something...'/>
+      <FontAwesomeIcon className='icon' icon='times' size='1x' onClick={clearSearch} />
+    </div>
   )
 }
 
